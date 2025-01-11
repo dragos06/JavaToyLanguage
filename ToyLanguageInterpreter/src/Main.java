5a7540fe76fd9abd0c3701998f002ec762624281
Dragoș Șerban <dragos4002@gmail.com>
2025-01-11 14:18:27 +0200

    New iteration of the Interpreter
    Goal: Enhance the interpreter to support file operations, improve expression evaluation, and add logging for better tracking of program execution.
    
        Features Developed:
            File Operations: Integrated the ability to read from and write to files within the interpreter. This allowed programs to perform file-based input/output operations, such as printing values to an output file.
            Expression Enhancements: Added more complex expressions, such as relational and logical expressions, expanding the interpreter’s capabilities.
            Repository Management: Improved the Repository class to manage multiple PrgState instances, track execution states, and handle program output and logging.
            Logging: Implemented logging mechanisms to capture program execution steps, helping in debugging and tracking the state at each step of execution.
            Menu System: Developed a simple menu system that allowed the user to load programs, run them, and view the output.
    
        Outcome: A more feature-complete interpreter capable of handling complex expressions and file operations, with logging functionality for better program state tracking.

--- ToyLanguageInterpreter/src/Main.java
+++ ToyLanguageInterpreter/src/Main.java
@@ -1,14 +1,26 @@
+import model.expressions.ArithmeticalExpression;
+import model.expressions.AritmeticalOperator;
+import model.expressions.ValueExpression;
+import model.expressions.VariableExpression;
+import model.statement.*;
+import model.types.MyBoolType;
+import model.types.MyIntType;
+import model.types.StringType;
+import model.value.MyBoolValue;
+import model.value.MyIntValue;
+import model.value.StringValue;
+import org.w3c.dom.Text;
 import repository.Repository;
 import repository.IRepository;
 import controller.Controller;
+import view.Interpreter;
+import view.TextMenu;
 import view.View;
+import view.commands.ExitCommand;
+import view.commands.RunExampleCommand;
 
 public class Main {
     public static void main(String[] args) {
-        IRepository repository = new Repository();
-        Controller controller = new Controller(repository);
-        View view = new View(controller);
-
-        view.run();
+        Interpreter.main();
     }
 }
