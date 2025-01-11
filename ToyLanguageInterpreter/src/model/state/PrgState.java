6ceff3dfd6c26d4130d0d496c59108219772e129
Dragoș Șerban <dragos4002@gmail.com>
2025-01-11 14:22:04 +0200

    Added necessary getters and setters, as well a toString for the FileTable

--- ToyLanguageInterpreter/src/model/state/PrgState.java
+++ ToyLanguageInterpreter/src/model/state/PrgState.java
@@ -5,17 +5,24 @@ import model.adt.MyIStack;
 import model.adt.MyIList;
 import model.statement.MyIStatement;
 import model.value.MyIValue;
+import model.value.StringValue;
+
+import java.io.BufferedReader;
 
 public class PrgState {
     private MyIDictionary<String, MyIValue> symTable;
     private MyIStack<MyIStatement> exeStack;
     private MyIList<String> output;
+    private MyIStatement initialState;
+    private MyIDictionary<StringValue, BufferedReader> fileTable;
 
-    public PrgState(MyIDictionary<String, MyIValue> symTable, MyIStack<MyIStatement> exeStack, MyIList<String> output, MyIStatement initialState) {
+    public PrgState(MyIDictionary<String, MyIValue> symTable, MyIStack<MyIStatement> exeStack, MyIList<String> output, MyIStatement initialState, MyIDictionary<StringValue, BufferedReader> fileTable) {
         this.symTable = symTable;
         this.exeStack = exeStack;
         this.output = output;
-        this.exeStack.push(initialState);
+        this.fileTable = fileTable;
+        this.initialState = initialState.deepCopy();
+        this.exeStack.push(this.initialState);
     }
 
     public MyIStack<MyIStatement> getExeStack() {
@@ -28,6 +35,31 @@ public class PrgState {
 
     public MyIList<String> getOutput() {
         return output;
+    }
+
+    public MyIStatement getInitialState() {
+        return initialState;
+    }
+
+    public void setInitialState(MyIStatement initialState) {
+        this.initialState = initialState;
+    }
+
+    public MyIDictionary<StringValue, BufferedReader> getFileTable() {
+        return fileTable;
+    }
+
+    public void setFileTable(MyIDictionary<StringValue, BufferedReader> fileTable) {
+        this.fileTable = fileTable;
+    }
+
+    public String fileTableToString() {
+        StringBuilder text = new StringBuilder();
+        text.append("FileTable: \n");
+        for (StringValue key : this.fileTable.getKeys()) {
+            text.append(key).append("\n");
+        }
+        return text.toString();
     }
 
     public void setSymTable(MyIDictionary<String, MyIValue> symTable) {
@@ -44,6 +76,6 @@ public class PrgState {
 
     @Override
     public String toString() {
-        return symTable.toString() + " " + exeStack.toString() + " " + output.toString();
+        return symTable.toString() + "\n" + exeStack.toString() + "\n" + output.toString() + "\n" + this.fileTableToString() + "\n-----------------------------\n";
     }
 }
