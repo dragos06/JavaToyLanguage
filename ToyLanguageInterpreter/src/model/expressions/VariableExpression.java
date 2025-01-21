8393ad814f9328692a273bc01903d55d0e7b1eac
Dragoș Șerban <dragos4002@gmail.com>
2025-01-21 16:41:05 +0200

    Added HeapTable to eval

--- ToyLanguageInterpreter/src/model/expressions/VariableExpression.java
+++ ToyLanguageInterpreter/src/model/expressions/VariableExpression.java
@@ -3,6 +3,7 @@ package model.expressions;
 import exception.ADTException;
 import exception.ExpressionException;
 import model.adt.MyIDictionary;
+import model.adt.MyIHeap;
 import model.value.MyIValue;
 
 public class VariableExpression implements MyIExpression {
@@ -13,7 +14,7 @@ public class VariableExpression implements MyIExpression {
     }
 
     @Override
-    public MyIValue eval(MyIDictionary<String, MyIValue> sym_table) throws ADTException {
+    public MyIValue eval(MyIDictionary<String, MyIValue> sym_table, MyIHeap heap) throws ADTException {
         return sym_table.getValue(variable);
     }
 
