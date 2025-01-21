6dbbd63276f44816acd55f5451cfa93ae0a59f42
Dragoș Șerban <dragos4002@gmail.com>
2025-01-21 16:41:43 +0200

    Deleted methods referring to FileTable(implemented class)

--- ToyLanguageInterpreter/src/model/state/PrgState.java
+++ ToyLanguageInterpreter/src/model/state/PrgState.java
@@ -1,8 +1,6 @@
 package model.state;
 
-import model.adt.MyIDictionary;
-import model.adt.MyIStack;
-import model.adt.MyIList;
+import model.adt.*;
 import model.statement.MyIStatement;
 import model.value.MyIValue;
 import model.value.StringValue;
@@ -14,13 +12,15 @@ public class PrgState {
     private MyIStack<MyIStatement> exeStack;
     private MyIList<String> output;
     private MyIStatement initialState;
-    private MyIDictionary<StringValue, BufferedReader> fileTable;
+    private IFileTable<StringValue, BufferedReader> fileTable;
+    private MyIHeap heapTable;
 
-    public PrgState(MyIDictionary<String, MyIValue> symTable, MyIStack<MyIStatement> exeStack, MyIList<String> output, MyIStatement initialState, MyIDictionary<StringValue, BufferedReader> fileTable) {
+    public PrgState(MyIDictionary<String, MyIValue> symTable, MyIStack<MyIStatement> exeStack, MyIList<String> output, MyIStatement initialState, FileTable<StringValue, BufferedReader> fileTable, MyIHeap heapTable) {
         this.symTable = symTable;
         this.exeStack = exeStack;
         this.output = output;
         this.fileTable = fileTable;
+        this.heapTable = heapTable;
         this.initialState = initialState.deepCopy();
         this.exeStack.push(this.initialState);
     }
@@ -41,41 +41,16 @@ public class PrgState {
         return initialState;
     }
 
-    public void setInitialState(MyIStatement initialState) {
-        this.initialState = initialState;
-    }
-
-    public MyIDictionary<StringValue, BufferedReader> getFileTable() {
+    public IFileTable<StringValue, BufferedReader> getFileTable() {
         return fileTable;
     }
 
-    public void setFileTable(MyIDictionary<StringValue, BufferedReader> fileTable) {
-        this.fileTable = fileTable;
-    }
-
-    public String fileTableToString() {
-        StringBuilder text = new StringBuilder();
-        text.append("FileTable: \n");
-        for (StringValue key : this.fileTable.getKeys()) {
-            text.append(key).append("\n");
-        }
-        return text.toString();
-    }
-
-    public void setSymTable(MyIDictionary<String, MyIValue> symTable) {
-        this.symTable = symTable;
-    }
-
-    public void setExeStack(MyIStack<MyIStatement> exeStack) {
-        this.exeStack = exeStack;
-    }
-
-    public void setOutput(MyIList<String> output) {
-        this.output = output;
+    public MyIHeap getHeapTable() {
+        return heapTable;
     }
 
     @Override
     public String toString() {
-        return symTable.toString() + "\n" + exeStack.toString() + "\n" + output.toString() + "\n" + this.fileTableToString() + "\n-----------------------------\n";
+        return symTable.toString() + "\n" + exeStack.toString() + "\n" + output.toString() + "\n" + fileTable.toString() + "\n" + heapTable.toString() + "\n-----------------------------\n";
     }
 }
